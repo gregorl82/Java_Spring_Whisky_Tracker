1d913136646782c6f7d8d29fc4a2fa887d1c839d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-02-12 16:38:11 +0000

    Adds findByYear method to WhiskyController

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,32 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/whiskies")
 public class WhiskyController {
+
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies() {
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/year")
+    public ResponseEntity<List<Whisky>> findByYear(@RequestParam(name = "year") int year){
+        List<Whisky> foundWhiskies = whiskyRepository.findByYear(year);
+        return new ResponseEntity<>(foundWhiskies, HttpStatus.OK);
+    }
 
 }
